4b6c450a0c1e175b1e98bacc34639ff0c7e4292a
dacixn <77625708+dacixn@users.noreply.github.com>
2021-11-12 01:45:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# AT LEAST 2 OF THE VARIABLES HAVE TO BE DEFINED TO WORK, INSERT YOUR INFORMATION HERE
+
+# DO NOT DEFINE ANY OF THE VARIABLES AS ZERO, YOU WILL GET AN ERROR. 
+
+# LEAVE THE NUMBERS YOU ARE NOT WORKING WITH TO 1, IT WILL NOT IMPACT YOUR RESULTS.
+
+density = 1 
+mass = 1
+volume = 1
+
+# DO NOT TOUCH THE "CALCULATE" VARIABLES
+
+calculate_density = mass / volume
+calculate_mass = volume * density
+calculate_volume = mass / density
+
+# DENSITY IS CALCULATED IN g/cm3
+# YOUR OPTIONS TO CALCULATE ARE:
+# print(calculate_density)
+# print(calculate_mass)
+# print(calculate_volume)
+
+print(calculate_density)
+
+# ENTER THE VARIABLE THAT DEFINES WHAT YOU WANT TO CALCULATE BETWEEN THE BRACKETS
+
+# YOUR ANSWER WILL SHOW UP IN THE CONSOLE WHEN YOU PRESS "RUN"
